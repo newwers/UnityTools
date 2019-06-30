af8801e3b3510a9b5bf78762b8e167884984817e
newwer <1554263906@qq.com>
2019-06-30 17:59:05 +0800

    添加Vector向量的扩展函数parse进行解析

---
+++ Tools/Assets/__MyScripts/UnityScriptExtend/Vector2Extend.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector2Extend  {
+
+    public static Vector2 Parse(this Vector2 vec, string text)
+    {
+        string[] vector = text.Split(',');
+        float x, y = 0f;
+        if (float.TryParse(vector[0], out x) && float.TryParse(vector[1], out y))
+        {
+            return new Vector2(x, y);
+        }
+        return new Vector2();
+    }
+}
---
+++ Tools/Assets/__MyScripts/UnityScriptExtend/Vector3Extend.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 扩展Vector3类的函数
+/// </summary>
+public static class Vector3Extend  {
+
+	public static Vector3 Parse(this Vector3 vec,string text)
+    {
+        string[] vector = text.Split(',');
+        float x,y,z = 0f;
+        if (float.TryParse(vector[0],out x) && float.TryParse(vector[1], out y) && float.TryParse(vector[2], out z))
+        {
+            return new Vector3(x,y,z);
+        }
+        return new Vector3();
+    }
+
+}
