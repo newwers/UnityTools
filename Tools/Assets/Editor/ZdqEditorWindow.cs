c5118b2ff9c4eca16d0ef1af084854f2bc028145
newwer <1554263906@qq.com>
2019-11-11 17:22:21 +0800

    add unity editor style

--- Tools/Assets/Editor/ZdqEditorWindow.cs
+++ Tools/Assets/Editor/ZdqEditorWindow.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class ZdqEditorWindow : EditorWindow {
+public class ZdqEditorWindow : EditorWindow
+{
 
     [MenuItem("zdq/工具window1(固定大小)")]
     public static void ShowWindow1()
@@ -26,13 +27,45 @@ public class ZdqEditorWindow : EditorWindow {
 
     private Vector2Int size;
 
+    bool m_IsShowTestToggle = false;
     private void OnGUI()
     {
         //绘制窗体
-        #region 设置名字
+
+        //设置选中物品名字
+        SetSelectItemsName();
+        //设置UILanguageSprite的Size
+        SetUILanguageSpriteSize();
+        //一键创建lua模块文件夹和文件
+        CreateLuaModuleFolder();
 
         
-        EditorGUILayout.BeginHorizontal();
+        
+
+        //文本框显示鼠标再窗口的位置
+        //EditorGUILayout.LabelField("鼠标在窗口的位置:", Event.current.mousePosition.ToString());
+
+        if (GUILayout.Button("关闭窗口"))
+        {
+            //关闭窗口
+            this.Close();
+        }
+
+        m_IsShowTestToggle = GUILayout.Toggle(m_IsShowTestToggle, "是否显示所有样式");
+        if (m_IsShowTestToggle == true)
+        {
+            TestGUIStyle();
+        }
+    }
+    /// <summary>
+    /// 设置选中物品名字
+    /// </summary>
+    void SetSelectItemsName()
+    {
+        #region 设置名字
+
+
+        EditorGUILayout.BeginHorizontal(new GUIStyle("HelpBox"));
         //输入框控件
         renameText = EditorGUILayout.TextField("重命名名字：", renameText);
 
@@ -86,11 +119,16 @@ public class ZdqEditorWindow : EditorWindow {
         }
         EditorGUILayout.EndHorizontal();
         #endregion
-
+    }
+    /// <summary>
+    /// 设置UILanguageSprite的Size
+    /// </summary>
+    void SetUILanguageSpriteSize()
+    {
         #region 修改属性
 
-        
-        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.BeginHorizontal(new GUIStyle("HelpBox"));
 
         size = EditorGUILayout.Vector2IntField("要修改的坐标:", size);
 
@@ -107,7 +145,7 @@ public class ZdqEditorWindow : EditorWindow {
             Undo.RecordObjects(selectList, "selectList3");
             foreach (var item in selectList)
             {
-                //var sprite =  item.GetComponent<UILanguageSprite>();
+                //var sprite = item.GetComponent<UILanguageSprite>();
                 //sprite.width = size.x;
                 //sprite.height = size.y;
             }
@@ -116,13 +154,66 @@ public class ZdqEditorWindow : EditorWindow {
         EditorGUILayout.EndHorizontal();
 
         #endregion
-        //文本框显示鼠标再窗口的位置
-        EditorGUILayout.LabelField("鼠标在窗口的位置:", Event.current.mousePosition.ToString());
-
-        if (GUILayout.Button("关闭窗口"))
-        {
-            //关闭窗口
-            this.Close();
-        }
     }
+
+
+    /// <summary>
+    /// 一键创建lua模块文件夹和文件
+    /// </summary>
+    void CreateLuaModuleFolder()
+    {
+        GUIStyle style = new GUIStyle("HelpBox");
+        EditorGUILayout.BeginHorizontal(style);
+        string path = Application.dataPath + "/[Resources]/Lua/Module";
+
+
+
+        EditorGUILayout.EndHorizontal();
+    }
+    Vector2 scrollPosition = new Vector2(0, 0);
+    GUIStyle textStyle = new GUIStyle("HeaderLabel");
+    string search = "";
+    void TestGUIStyle()
+    {
+        if (textStyle == null)
+        {
+            textStyle = new GUIStyle("HeaderLabel");
+            textStyle.fontSize = 20;
+        }
+
+        GUILayout.BeginHorizontal("HelpBox");
+        GUILayout.Label("点击示例，可以将其名字复制下来", textStyle);
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("Search:");
+        search = EditorGUILayout.TextField(search);
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal("PopupCurveSwatchBackground");
+        GUILayout.Label("示例", textStyle, GUILayout.Width(300));
+        GUILayout.Label("名字", textStyle, GUILayout.Width(300));
+        GUILayout.EndHorizontal();
+
+
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+
+        foreach (var style in GUI.skin.customStyles)
+        {
+            if (style.name.ToLower().Contains(search.ToLower()))
+            {
+                GUILayout.Space(15);
+                GUILayout.BeginHorizontal("PopupCurveSwatchBackground");
+                if (GUILayout.Button(style.name, style, GUILayout.Width(300)))
+                {
+                    EditorGUIUtility.systemCopyBuffer = style.name;
+                    Debug.LogError(style.name);
+                }
+                EditorGUILayout.SelectableLabel(style.name, GUILayout.Width(300));
+                GUILayout.EndHorizontal();
+            }
+        }
+
+        GUILayout.EndScrollView();
+    }
+
+
 }
