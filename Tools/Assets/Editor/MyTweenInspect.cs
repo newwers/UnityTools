9e8eb7f4a162ff6cb78e347066d939efd97525f0
newwer <1554263906@qq.com>
2020-02-09 18:15:40 +0800

    提交自己做的过度脚本

---
+++ Tools/Assets/Editor/MyTweenInspect.cs
@@ -0,0 +1,72 @@
+﻿/*
+	newwer
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Asset.Core.Tools.ExpandEditor
+{
+    [CustomEditor(typeof(MyTween),true)]//true的意思是,当所有没有回调的编辑器都不匹配的时候才匹配此编辑器
+    [CanEditMultipleObjects]
+    /// <summary>
+    /// 扩展编辑器
+    /// </summary>
+	public class MyTweenInspect : Editor {
+
+        SerializedProperty _isScale;
+        SerializedProperty _scaleCurve;
+        SerializedProperty _moveTime;
+        SerializedProperty _from;
+        SerializedProperty _to;
+        SerializedProperty _positionCurve;
+
+        MyTween _myTween;
+
+        private void OnEnable()
+        {
+            if (serializedObject!=null)
+            {
+                _isScale=serializedObject.FindProperty("isScale");
+                _scaleCurve=serializedObject.FindProperty("Scale_curve");
+                _moveTime=serializedObject.FindProperty("Move_time");
+                _from = serializedObject.FindProperty("From");
+                _to = serializedObject.FindProperty("To");
+                _positionCurve = serializedObject.FindProperty("Position_curve");
+            }
+            _myTween=target as MyTween;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            DrawPositionProperty();
+
+            DrawScaleProperty();
+
+            serializedObject.ApplyModifiedProperties();//将做出的修改应用到原先的脚本中,不加这句则会造成修改不了属性的情况
+        }
+
+        private void DrawPositionProperty()
+        {
+            _myTween.Move_time = EditorGUILayout.FloatField("移动时间", _myTween.Move_time);
+            _myTween.From = EditorGUILayout.Vector3Field("位移开始点:", _myTween.From);
+            _myTween.To = EditorGUILayout.Vector3Field("位移结束点:", _myTween.To);
+            _myTween.Position_curve = EditorGUILayout.CurveField("位移曲线", _myTween.Position_curve);
+            //EditorGUILayout.PropertyField(_positionCurve);
+        }
+
+        private void DrawScaleProperty()
+        {
+            _myTween.isScale = EditorGUILayout.Toggle("是否缩放", _myTween.isScale);
+            if (_myTween.isScale)
+            {
+                
+                _myTween.Scale_curve = EditorGUILayout.CurveField("缩放曲线", _myTween.Scale_curve);
+            }
+        }
+
+    }
+}
---
+++ Tools/Assets/__MyScripts/Common/MyTween.cs
@@ -0,0 +1,96 @@
+﻿/*
+	newwer
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asset.Core.Tools
+{
+
+	public class MyTween : MonoBehaviour {
+
+        [Tooltip("位置过渡的起点")]
+        public Vector3 From;
+        [Tooltip("位置过渡的终点")]
+        public Vector3 To;
+        [Tooltip("控制移动时,挂载物体的大小")]
+        public AnimationCurve Position_curve;
+        [Tooltip("控制移动时,挂载物体的大小")]
+        public AnimationCurve Scale_curve;
+        [Tooltip("过渡的时间")]
+        public float Move_time;
+        [Tooltip("是否使用动画缩放")]
+        public bool isScale;
+
+        WaitForSeconds _waitForSecond;
+        float _scaleTime = 0f;
+        float _positionTime = 0f;
+        Coroutine _scaleCoroutine;
+        Coroutine _positionCoroutine;
+
+        private void Awake()
+        {
+            _waitForSecond = new WaitForSeconds(0.01f);
+            _positionCoroutine = StartCoroutine(PosotionTween());
+            if (isScale)
+            {
+                _scaleCoroutine = StartCoroutine(ScaleTween());
+            }
+        }
+
+        IEnumerator ScaleTween()
+        {
+            while (true)
+            {
+                
+                if (_scaleTime >= Move_time)
+                {
+                    _scaleTime = 0f;
+                    if (_scaleCoroutine!=null)
+                    {
+                        StopCoroutine(_scaleCoroutine);
+                    }
+
+
+                    break;//跳出while循环
+                }
+                float value = Scale_curve.Evaluate(_scaleTime / Move_time);
+                transform.localScale = new Vector3(value, value, value);
+                _scaleTime += 0.01f;
+                
+                yield return _waitForSecond;
+                
+            }
+            
+        }
+
+        IEnumerator PosotionTween()
+        {
+            while (true)
+            {
+                if (_positionTime >= Move_time)
+                {
+                    _positionTime = 0f;
+                    if (_positionCoroutine!=null)
+                    {
+                        StopCoroutine(_positionCoroutine);
+                    }
+                    break;
+                }
+
+                float value= Position_curve.Evaluate(_positionTime / Move_time);//获取曲线上的值
+
+                Vector3 lerpPos= Vector3.Lerp(From, To, (_positionTime / Move_time)*value);
+
+
+                transform.localPosition = lerpPos;
+                _positionTime += 0.01f;
+                yield return _waitForSecond;
+            }
+        }
+
+        
+
+    }
+}
