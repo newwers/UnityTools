9f97ef5de838b9838345bfb479dc5f8277fd95ef
DESKTOP-1NV8NIM\TZ03 <1554263906@qq.com>
2020-08-03 17:35:48 +0800

    添加鼠标模拟模块

---
+++ Tools/Assets/__MyScripts/InputManager/Simulation/GetInputHandle.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// 获取玩家输入操作,目前只支持PC,(Android 暂不支持)
+/// IPointerClickHandler 的形式,只适合unityUI上面进行的点击操作才有,这样会导致监听的问题,每个UI都挂载脚本吗?不然就用Input进行检测
+/// </summary>
+public class GetInputHandle : MonoBehaviour
+{
+
+    Queue<InputData> m_InputQueue = new Queue<InputData>();
+
+    /// <summary>
+    /// 点击开始点
+    /// </summary>
+    private Vector2 m_BeginClickPoint;
+    private POINT m_BeginClick_Point;
+
+    private float m_Timer;
+
+    /// <summary>
+    /// 是否处在模拟状态,用来禁用按键监听
+    /// </summary>
+    private bool m_IsSimulater;
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && m_IsSimulater == false)
+        {
+            m_BeginClickPoint = Input.mousePosition;
+            POINT point = default;
+            MouseSimulater.GetCursorPos(ref point);
+            m_BeginClick_Point = point;
+            //print("m_BeginClickPoint=" + m_BeginClickPoint);
+            m_Timer = Time.time;
+        }
+        if (Input.GetMouseButtonUp(0) && m_IsSimulater == false)
+        {
+            POINT point = default;
+            MouseSimulater.GetCursorPos(ref point);
+            
+            m_InputQueue.Enqueue(new InputData(InputType.Click, m_BeginClickPoint, Input.mousePosition, m_BeginClick_Point, point, Time.time - m_Timer));
+            print("Time.time - m_Timer=" + (Time.time - m_Timer));
+            //MouseSimulater.UnityScreenToWindowPos(Input.mousePosition);
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (m_InputQueue != null && m_InputQueue.Count >0)
+            {
+                m_IsSimulater = true;
+                //StartCoroutine(Simulater(m_InputQueue.Dequeue()));
+                StartCoroutine(SimulaterByWindow(m_InputQueue.Dequeue()));
+            }
+        }
+    }
+
+    /// <summary>
+    /// 通过unity和window坐标系进行转换后模拟鼠标
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    IEnumerator Simulater(InputData data)
+    {
+        Vector2 pointStart = MouseSimulater.UnityScreenToWindowPos(data.pointStart_Unity);
+        Vector2 pointEnd = MouseSimulater.UnityScreenToWindowPos(data.pointEnd_Unity);
+        MouseSimulater.MoveTo(pointStart.x, pointStart.y);
+        MouseSimulater.LeftClickDown();
+        yield return null;
+        for (int i = 1; i <= 30; i++)
+        {
+            Vector2 point = Vector2.Lerp(pointStart, pointEnd, i / 30f);
+            MouseSimulater.MoveTo(point.x, point.y);
+            yield return data.pressTime / 30;
+        }
+        MouseSimulater.LeftClickUp();
+        yield return null;
+        m_IsSimulater = false;
+    }
+
+    /// <summary>
+    /// 通过直接用window坐标系进行模拟
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    IEnumerator SimulaterByWindow(InputData data)
+    {
+        Vector2 pointStart = new Vector2(data.pointStart_Window.X, data.pointStart_Window.Y);
+        Vector2 pointEnd = new Vector2(data.pointEnd_Window.X, data.pointEnd_Window.Y);
+        MouseSimulater.MoveTo(pointStart.x, pointStart.y);
+        MouseSimulater.LeftClickDown();
+        yield return null;
+        for (int i = 1; i <= 30; i++)
+        {
+            Vector2 point = Vector2.Lerp(pointStart, pointEnd, i / 30f);
+            MouseSimulater.MoveTo(point.x, point.y);
+            yield return data.pressTime / 30;
+        }
+        MouseSimulater.LeftClickUp();
+        yield return null;
+        m_IsSimulater = false;
+    }
+
+
+}
+
+public struct InputData
+{
+    public InputType inputType;
+    /// <summary>
+    /// 鼠标按下位置
+    /// </summary>
+    public Vector2 pointStart_Unity;
+    /// <summary>
+    /// 鼠标松开位置
+    /// </summary>
+    public Vector2 pointEnd_Unity;
+
+    /// <summary>
+    /// 鼠标按下位置(基于1920*1080分辨率)
+    /// </summary>
+    public POINT pointStart_Window;
+    /// <summary>
+    /// 鼠标松开位置(基于1920*1080分辨率)
+    /// </summary>
+    public POINT pointEnd_Window;
+
+    /// <summary>
+    /// 按下时间
+    /// </summary>
+    public float pressTime;
+
+
+    public InputData(InputType inputType, Vector2 pointStart_Unity, Vector2 pointEnd_Unity, POINT pointStart_Window, POINT pointEnd_Window, float pressTime)
+    {
+        this.inputType = inputType;
+        this.pointStart_Unity = pointStart_Unity;
+        this.pointEnd_Unity = pointEnd_Unity;
+        this.pointStart_Window = pointStart_Window;
+        this.pointEnd_Window = pointEnd_Window;
+        this.pressTime = pressTime;
+    }
+
+    //todo:考虑到多点触控
+    //可以参考 PointerEventData
+
+}
+
+public enum InputType
+{
+    None,
+    Click,
+}
---
+++ Tools/Assets/__MyScripts/InputManager/Simulation/MouseSimulater.cs
@@ -0,0 +1,227 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MouseSimulater
+{
+    #region DLLs
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern int SetCursorPos(int x, int y);
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern void mouse_event(MouseEventFlag dwFlags, int dx, int dy, uint dwData, UIntPtr dwExtraInfo);
+ 
+    // 方法参数说明
+    // VOID mouse_event(
+    //     DWORD dwFlags,         // motion and click options
+    //     DWORD dx,              // horizontal position or change
+    //     DWORD dy,              // vertical position or change
+    //     DWORD dwData,          // wheel movement
+    //     ULONG_PTR dwExtraInfo  // application-defined information
+    // );
+ 
+    [Flags]
+    enum MouseEventFlag : uint
+    {
+        Move = 0x0001,
+        LeftDown = 0x0002,
+        LeftUp = 0x0004,
+        RightDown = 0x0008,
+        RightUp = 0x0010,
+        MiddleDown = 0x0020,
+        MiddleUp = 0x0040,
+        XDown = 0x0080,
+        XUp = 0x0100,
+        Wheel = 0x0800,
+        VirtualDesk = 0x4000,
+        Absolute = 0x8000
+    }
+
+    /// <summary>
+    /// 获取当前窗体的位置
+    /// </summary>
+    /// <param name="hwnd">窗体句柄</param>
+    /// <param name="lpRect">存放窗体位置的结构</param>
+    /// <returns></returns>
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+
+    /// <summary>
+    /// 获取窗口句柄
+    /// </summary>
+    /// <returns></returns>
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    /// <summary>
+    /// 获得鼠标在屏幕上的位置
+    /// </summary>
+    /// <param name="lpPoint"></param>
+    /// <returns></returns>
+    [DllImport("user32.dll")]
+    // GetCursorPos() makes everything possible
+    public static extern bool GetCursorPos(ref POINT lpPoint);
+
+    #endregion
+
+    // Unity屏幕坐标从左下角开始，向右为X轴，向上为Y轴
+    // Windows屏幕坐标从左上角开始，向右为X轴，向下为Y轴
+
+    /// <summary>
+    /// 移动鼠标到指定位置（使用Unity屏幕坐标而不是Windows屏幕坐标）
+    /// </summary>
+    public static bool MoveTo(float x, float y)
+    {
+        if (x < 0 || y < 0 || x > 1920 || y > 1080)
+            return false;
+ 
+        SetCursorPos((int)x, (int)y);
+        return true;
+    }
+
+    /// <summary>
+    /// unity内的坐标系转换到屏幕坐标系
+    /// //1.获得当前显示器的分辨率(假设1920*1080)
+    /// //2.获得unity的窗体大小(Screen.width,Screen.height)
+    /// //3.获取窗体位置 GetWindowRect
+    /// //4.将鼠标在unity内点击的坐标转化成 0到窗体分辨率大小的取值范围
+    /// //5.将窗体位置 + 转化后点击位置的一半 = 屏幕目标位置
+    /// </summary>
+    /// <param name="unityPos"></param>
+    /// <returns></returns>
+    public static Vector2 UnityScreenToWindowPos(Vector2 unityPos)
+    {
+        Vector2 windowScreen = new Vector2(1920, 1080);
+
+        RECT windowPos;
+        GetWindowRect(GetActiveWindow(), out windowPos);
+
+        //unity中点击的位置  + (窗体位置 - unity窗体x的一半) = 点击屏幕上的位置
+
+        Vector2 windowPosVector2 = new Vector2(windowPos.Left + Screen.width /2, windowPos.Top + Screen.height /2);
+
+        float x = unityPos.x + windowPosVector2.x - Screen.width / 2;
+        float y = Screen.height/2 - unityPos.y + windowPosVector2.y;
+
+        Vector2 targetPos = new Vector2(x,y);
+
+        Debug.Log("unityPos=" + unityPos +",targetPos=" + targetPos + ",windowPosVector2=" + windowPosVector2 + ",windowPos.Left=" + windowPos.Left + ",Top=" + windowPos.Top);
+
+        return targetPos;
+    }
+ 
+    // 左键单击
+    public static void LeftClick(float x = -1, float y = -1)
+    {
+        if (MoveTo(x, y))
+        {
+            mouse_event(MouseEventFlag.LeftDown, 0, 0, 0, UIntPtr.Zero);
+            mouse_event(MouseEventFlag.LeftUp, 0, 0, 0, UIntPtr.Zero);
+        }
+    }
+
+    public static void LeftClickDown()
+    {
+        mouse_event(MouseEventFlag.LeftDown, 0, 0, 0, UIntPtr.Zero);
+    }
+
+    public static void LeftClickUp()
+    {
+        mouse_event(MouseEventFlag.LeftUp, 0, 0, 0, UIntPtr.Zero);
+    }
+
+    // 右键单击
+    public static void RightClick(float x = -1, float y = -1)
+    {
+        if (MoveTo(x, y))
+        {
+            mouse_event(MouseEventFlag.RightDown, 0, 0, 0, UIntPtr.Zero);
+            mouse_event(MouseEventFlag.RightUp, 0, 0, 0, UIntPtr.Zero);
+        }
+    }
+
+    public static void RightClickDown()
+    {
+        mouse_event(MouseEventFlag.RightDown, 0, 0, 0, UIntPtr.Zero);
+    }
+
+    public static void RightClickUp()
+    {
+        mouse_event(MouseEventFlag.RightUp, 0, 0, 0, UIntPtr.Zero);
+    }
+
+    // 中键单击
+    public static void MiddleClick(float x = -1, float y = -1)
+    {
+        if (MoveTo(x, y))
+        {
+            mouse_event(MouseEventFlag.MiddleDown, 0, 0, 0, UIntPtr.Zero);
+            mouse_event(MouseEventFlag.MiddleUp, 0, 0, 0, UIntPtr.Zero);
+        }
+    }
+ 
+   
+ 
+    // 中键按下
+    public static void MiddleDown()
+    {
+       mouse_event(MouseEventFlag.MiddleDown, 0, 0, 0, UIntPtr.Zero);
+    }
+ 
+    // 中键抬起
+    public static void MiddleUp()
+    {
+       mouse_event(MouseEventFlag.MiddleUp, 0, 0, 0, UIntPtr.Zero);
+    }
+ 
+    // 滚轮滚动
+    public static void ScrollWheel(float value)
+    {
+        mouse_event(MouseEventFlag.Wheel, 0, 0, (uint)value, UIntPtr.Zero);
+    }
+}
+
+public struct RECT
+{
+    /// <summary>
+    /// 窗体的左上角的点到屏幕最左边的x距离
+    /// </summary>
+    public int Left;        // x position of upper-left corner
+                            /// <summary>
+                            /// 窗体左上角的点到屏幕最上面的y轴距离
+                            /// </summary>
+    public int Top;         // y position of upper-left corner
+                            /// <summary>
+                            /// 窗体右下角的点到屏幕最右边的x轴距离
+                            /// </summary>
+    public int Right;       // x position of lower-right corner
+                            /// <summary>
+                            /// 窗体右下角的点到屏幕最右边的Y轴距离
+                            /// </summary>
+    public int Bottom;      // y position of lower-right corner
+}
+
+public struct POINT
+{
+    public int X;
+    public int Y;
+
+    public POINT(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    public override string ToString()
+    {
+        return ("X:" + X + ", Y:" + Y);
+    }
+
+    public bool Equal(POINT point)
+    {
+        if (this.X == point.X && this.Y == point.Y)
+        {
+            return true;
+        }
+        return false;
+    }
+}
