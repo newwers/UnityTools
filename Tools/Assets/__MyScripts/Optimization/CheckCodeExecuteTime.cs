c0f900f339139d2d7fdee33b56461b4a6e9d1302
DESKTOP-1NV8NIM\TZ03 <1554263906@qq.com>
2020-10-30 14:46:55 +0800

    修复代码执行时间脚本时间会累积的问题

--- Tools/Assets/__MyScripts/Optimization/CheckCodeExecuteTime.cs
+++ Tools/Assets/__MyScripts/Optimization/CheckCodeExecuteTime.cs
@@ -7,20 +7,22 @@ using System.Diagnostics;
 /// <summary>
 /// 检查代码执行时间
 /// </summary>
-public static class CheckCodeExecuteTime  {
+public static class CheckCodeExecuteTime
+{
 
-	private static Stopwatch stopwatch = new Stopwatch();
+    private static Stopwatch stopwatch = new Stopwatch();
 
     public static void StartCheck()
     {
+        stopwatch.Reset();
         stopwatch.Start(); //  开始监视代码运行时间
     }
 
-    public static void EndCheck()
+    public static void EndCheck(string name = "")
     {
         stopwatch.Stop(); //  停止监视
         TimeSpan timespan = stopwatch.Elapsed; //  获取当前实例测量得出的总时间
         double milliseconds = timespan.TotalMilliseconds;  //  总毫秒数
-        UnityEngine.Debug.Log("<color=#ff0000>代码执行时间:" + milliseconds + "毫秒</color>");
+        UnityEngine.Debug.Log("<color=#ff0000>代码执行时间:" + milliseconds + "毫秒</color> " + name);
     }
 }
