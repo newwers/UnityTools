57273fb4cdab5ed4e46facc7eb586a0701c2f43f
DESKTOP-1NV8NIM\TZ03 <1554263906@qq.com>
2020-11-03 19:19:07 +0800

    多语言脚本

---
+++ Tools/Assets/__MyScripts/Localization/LocalizationManager.cs
@@ -0,0 +1,143 @@
+﻿/********************************************************************
+生成日期:	2020-06-12
+类    名: 	LocalizationManager
+作    者:	zdq
+描    述:	多语言管理器
+*********************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using TopGame.Data;
+using UnityEngine;
+
+namespace TopGame.Core
+{
+    public class LocalizationManager : Module.IModule
+    {
+        public enum ELanguageType
+        {
+            /// <summary>
+            /// 无
+            /// </summary>
+            None,
+            /// <summary>
+            /// 汉语
+            /// </summary>
+            CN,
+            /// <summary>
+            /// 英语
+            /// </summary>
+            EN,
+            Count,
+        }
+
+        public delegate void OnLanguageChange(ELanguageType languageType);
+        public OnLanguageChange OnLanguageChangeCallback;
+
+        public ELanguageType CurrentLanguage
+        {
+            get
+            {
+                return m_eLanguageType;
+            }
+        }
+
+        Dictionary<uint, List<string>> m_vLanguageDic;
+
+        ELanguageType m_eLanguageType;
+
+        public void Awake()
+        {
+            var cfg = SystemSettingManager.getInstance().GetSystemSettingData();
+            if (cfg != null)
+            {
+                if (cfg.Language >= (int)ELanguageType.Count)
+                {
+                    m_eLanguageType = ELanguageType.CN;//默认中文
+                }
+                else
+                {
+                    m_eLanguageType = (ELanguageType)cfg.Language;
+                }
+            }
+            else
+            {
+                m_eLanguageType = ELanguageType.CN;//默认中文
+            }
+            SetCurrentLanguage(m_eLanguageType);
+
+            m_vLanguageDic = new Dictionary<uint, List<string>>();
+            var datas = DataManager.getInstance().Text.datas;
+
+            CsvData_Text.TextData data;
+            List<string> tempList;
+            foreach (var dataID in datas.Keys)
+            {
+                data = DataManager.getInstance().Text.GetData(dataID);
+                if (data != null)
+                {
+                    tempList = new List<string>();
+                    tempList.Add(data.textCN);
+                    tempList.Add(data.textEN);//如果新增语言类型,这边要新增对应字段,要注意数组添加顺序,会影响到后面读取
+                    m_vLanguageDic.Add(dataID, tempList);
+                }
+            }
+        }
+        //------------------------------------------------------
+        public void Destroy()
+        {
+            if (m_vLanguageDic != null)
+            {
+                m_vLanguageDic.Clear();
+            }
+            if (OnLanguageChangeCallback != null)
+            {
+                OnLanguageChangeCallback = null;
+            }
+        }
+        //------------------------------------------------------
+        public void SetCurrentLanguage(ELanguageType languageType)
+        {
+            if (m_eLanguageType != languageType)
+            {
+                if (languageType >= ELanguageType.Count)
+                {
+                    Plugin.Logger.Error("设置的语言类型不对:" + languageType);
+                    return;
+                }
+                m_eLanguageType = languageType;
+                if (OnLanguageChangeCallback != null)
+                {
+                    OnLanguageChangeCallback.Invoke(m_eLanguageType);
+                }
+            }
+        }
+        //------------------------------------------------------
+        public string GetLocalization(uint id)
+        {
+            List<string> tempList = new List<string>();
+            if (m_vLanguageDic.TryGetValue(id, out tempList))
+            {
+                int index = ((int)m_eLanguageType) -1;//根据枚举类型id,取对应语言,-1是枚举第一个类型是None
+                if (tempList.Count > index)
+                {
+                    return tempList[index];
+                }
+            }
+            return null;
+        }
+        //------------------------------------------------------
+        public uint GetIDByChinese(string str)
+        {
+            foreach (var key in m_vLanguageDic.Keys)
+            {
+                if (m_vLanguageDic[key][0].Equals(str))
+                {
+                    return key;
+                }
+            }
+            return 0;
+        }
+    }
+
+}
---
+++ Tools/Assets/__MyScripts/Localization/LocalizationText.cs
@@ -0,0 +1,172 @@
+﻿/********************************************************************
+生成日期:	2020-06-12
+类    名: 	LocalizationText
+作    者:	zdq
+描    述:	多语言控制Text显示组件,使用方法,将此脚本挂载到显示的Text控件上
+*********************************************************************/
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+using TopGame.Data;
+#endif
+
+namespace TopGame.Core
+{
+    [RequireComponent(typeof(Text))]
+    public class LocalizationText : MonoBehaviour
+    {
+        public uint ID = 0;
+
+        Text m_text;
+
+        private void Awake()
+        {
+            m_text = GetComponent<Text>();
+        }
+        //------------------------------------------------------
+        private void OnEnable()
+        {
+            RefreshText();
+        }
+        //------------------------------------------------------
+        private void Start()
+        {
+            GameInstance.getInstance().localizationMgr.OnLanguageChangeCallback += OnLanguageChangeCallback;
+        }
+        //------------------------------------------------------
+        private void OnLanguageChangeCallback(LocalizationManager.ELanguageType languageType)
+        {
+            if (m_text == null ||gameObject.activeInHierarchy == false)
+            {
+                return;
+            }
+            RefreshText();
+        }
+
+        /// <summary>
+        /// 提供一个设置id的方法,同时刷新显示
+        /// </summary>
+        /// <param name="id"></param>
+        public void UpdateLocalization(uint id)
+        {
+            ID = id;
+            RefreshText();
+        }
+
+        //------------------------------------------------------
+        [ContextMenu("测试显示")]
+        private void RefreshText()
+        {
+            if (m_text == null || ID == 0)
+            {
+                return;
+            }
+#if UNITY_EDITOR
+            if (Application.isPlaying == false)
+                return;
+#endif
+            if (GameInstance.getInstance().localizationMgr == null) return;
+            string text = GameInstance.getInstance().localizationMgr.GetLocalization(ID);
+            if (text != null)
+            {
+                m_text.text = text;
+            }
+        }
+#if UNITY_EDITOR
+
+        //------------------------------------------------------
+        [ContextMenu("根据中文获取多语言ID")]
+        private void GetIDByText()
+        {
+            Text text = GetComponent<Text>();
+            if (text == null)
+            {
+                return;
+            }
+
+            
+            var csv = DataManager.getInstance().Text;
+            if (csv == null)
+            {
+                Data.DataManager.ReInit();
+                csv = DataManager.getInstance().Text;
+            }
+            if (csv == null)
+            {
+                Plugin.Logger.Error("Csv数据为null");
+                return;
+            }
+            var datas = csv.datas;
+
+            foreach (var dataID in datas.Keys)
+            {
+                var data = DataManager.getInstance().Text.GetData(dataID);
+                if (data != null)
+                {
+                    if (data.textCN.Equals(text.text))
+                    {
+                        ID = dataID;
+                        break;
+                    }
+                }
+            }
+
+            UnityEditor.EditorUtility.SetDirty(gameObject);
+            UnityEditor.AssetDatabase.SaveAssets();
+        }
+
+        //------------------------------------------------------
+        [ContextMenu("根据多语言ID获取中文")]
+        private void GetChineseByID()
+        {
+            Text text = GetComponent<Text>();
+            if (text == null)
+            {
+                return;
+            }
+
+
+
+            var csv = DataManager.getInstance().Text;
+            if (csv == null)
+            {
+                Data.DataManager.ReInit();
+                csv = DataManager.getInstance().Text;
+            }
+            if (csv == null)
+            {
+                Plugin.Logger.Error("Csv数据为null");
+                return;
+            }
+            var datas = csv.datas;
+
+            foreach (var dataID in datas.Keys)
+            {
+                var data = DataManager.getInstance().Text.GetData(dataID);
+                if (data != null)
+                {
+                    if (data.id == ID)
+                    {
+                        text.text = data.textCN;
+                        break;
+                    }
+                }
+            }
+
+            UnityEditor.EditorUtility.SetDirty(gameObject);
+            UnityEditor.AssetDatabase.SaveAssets();
+        }
+#endif
+        //------------------------------------------------------
+        private void OnDestroy()
+        {
+            if (Module.ModuleManager.mainModule == null)
+            {
+                return;
+            }
+            GameInstance.getInstance().localizationMgr.OnLanguageChangeCallback -= OnLanguageChangeCallback;
+        }
+    }
+}
