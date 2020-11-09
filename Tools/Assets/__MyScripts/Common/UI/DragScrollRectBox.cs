611912ac11359b5a76bd1d2adcfc33345793b00b
DESKTOP-1NV8NIM\TZ03 <1554263906@qq.com>
2020-11-09 10:01:57 +0800

    脚本位置整理,添加允许点击和拖拽子物体脚本

---
+++ Tools/Assets/__MyScripts/Common/UI/DragScrollRectBox.cs
@@ -0,0 +1,51 @@
+﻿/********************************************************************
+生成日期:	6:3:2020 14:06
+类    名: 	MultiSelectBtnBox
+作    者:	JaydenHe
+描    述:	允许ScrollRect子物体点击拖动
+*********************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace TopGame.UI
+{
+    public class DragScrollRectBox : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+    {
+        public ScrollRect DragScorll;
+        public void Awake()
+        {
+            if (DragScorll == null)
+                DragScorll = transform.GetComponentInParent<ScrollRect>();
+        }
+        //------------------------------------------------------
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            if (DragScorll != null)
+            {
+                DragScorll.OnBeginDrag(eventData);
+            }
+        }
+        //------------------------------------------------------
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (DragScorll != null)
+            {
+                DragScorll.OnDrag(eventData);
+            }
+        }
+        //------------------------------------------------------
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            if (DragScorll != null)
+            {
+                DragScorll.OnEndDrag(eventData);
+            }
+        }
+        //------------------------------------------------------
+    }
+}
+
