09d74ee4b4656348b15293c85f2873fa0269f75e
DESKTOP-1NV8NIM\TZ03 <1554263906@qq.com>
2020-11-13 14:16:42 +0800

    血条过度脚本修复第一次显示总是从1开始过度的问题

--- Tools/Assets/__MyScripts/Common/Tween/SliderLerp.cs
+++ Tools/Assets/__MyScripts/Common/Tween/SliderLerp.cs
@@ -9,172 +9,181 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SliderLerp : MonoBehaviour
+namespace TopGame.UI
 {
-
-    public Image BackSlider;
-    public Component FrontSlider;//兼容 Image 类型 和 Scrollbar 类型
-
-    [Header("插值速度,单位秒")]
-    public float LerpSpeed = 1f;
-
-    float m_CurBackValue = 1f;
-    float m_CurFrontValue = 1f;
-
-    [Header("插值次数,次数越大越平滑")]
-    public int LerpCount = 30;
-
-
-    Coroutine m_LerpCoroutine;
-
-    Image m_Front_Image;
-    Scrollbar m_Front_Scrollbar;
-
-    float m_SliderValue = 1f;
-
-
-    /// <summary>
-    /// 设置数值
-    /// </summary>
-    /// <param name="newValue"></param>
-    public void SetValue(float newValue)
+    public class SliderLerp : MonoBehaviour
     {
-        newValue = Mathf.Clamp01(newValue);
 
-        if (m_LerpCoroutine != null)
-        {
-            StopCoroutine(m_LerpCoroutine);
-            m_LerpCoroutine = null;
-        }
+        public Image BackSlider;
+        public Component FrontSlider;//兼容 Image 类型 和 Scrollbar 类型
 
-        if (BackSlider == null || FrontSlider == null)
-        {
-            Debug.LogError("UI组件缺少,不进行过渡;BackSlider:" + BackSlider + ",FrontSlider:" + FrontSlider);
-            return;
-        }
+        [Header("插值速度,单位秒")]
+        public float LerpSpeed = 1f;
 
-        if (FrontSlider is Image)
-        {
-            m_Front_Image = FrontSlider as Image;
-        }
-        else if (FrontSlider is Scrollbar)
-        {
-            m_Front_Scrollbar = FrontSlider as Scrollbar;
-        }
+        float m_CurBackValue = 1f;
+        float m_CurFrontValue = 1f;
+        
+        [Header("插值次数,次数越大越平滑")]
+        public int LerpCount = 30;
 
-        ValueLerp(m_SliderValue, newValue);
-        m_SliderValue = newValue;
-    }
 
-    void ValueLerp(float oldValue, float newValue)
-    {
-        WaitForSeconds wait = new WaitForSeconds(LerpSpeed / (float)LerpCount);
+        Coroutine m_LerpCoroutine;
 
-        if (oldValue > newValue)
+        Image m_Front_Image;
+        Scrollbar m_Front_Scrollbar;
+
+        float m_SliderValue = 1f;
+
+        bool m_IsActive = false;
+
+
+        /// <summary>
+        /// 设置数值
+        /// </summary>
+        /// <param name="newValue"></param>
+        public void SetValue(float newValue,float oldValue = 1f)
         {
-            m_LerpCoroutine = StartCoroutine(SubValue(m_CurBackValue, newValue, wait));
-        }
-        else if (oldValue < newValue)
-        {
-            m_LerpCoroutine = StartCoroutine(AddValue(m_CurFrontValue, newValue, wait));
-        }
-        else
-        {
-            //数值相等情况,直接赋值
+            newValue = Mathf.Clamp01(newValue);
+
+            if (m_LerpCoroutine != null)
+            {
+                StopCoroutine(m_LerpCoroutine);
+                m_LerpCoroutine = null;
+            }
+
+            if (BackSlider == null || FrontSlider == null)
+            {
+                Plugin.Logger.Error("UI组件缺少,不进行过渡;BackSlider:" + BackSlider + ",FrontSlider:" + FrontSlider);
+                return;
+            }
+
             if (FrontSlider is Image)
             {
                 m_Front_Image = FrontSlider as Image;
-                m_Front_Image.fillAmount = newValue;
             }
             else if (FrontSlider is Scrollbar)
             {
                 m_Front_Scrollbar = FrontSlider as Scrollbar;
-                m_Front_Scrollbar.size = newValue;
             }
 
+            m_SliderValue = oldValue;
+            ValueLerp(m_SliderValue, newValue);
+            m_SliderValue = newValue;
+            m_IsActive = true;
+        }
+        //------------------------------------------------------
+        void ValueLerp(float oldValue,float newValue)
+        {
+            WaitForSeconds wait = new WaitForSeconds(LerpSpeed / (float)LerpCount);
+
+            if (oldValue > newValue)
+            {
+                m_LerpCoroutine = StartCoroutine(SubValue(m_CurBackValue, newValue, wait));
+            }
+            else if (oldValue < newValue)
+            {
+                m_LerpCoroutine = StartCoroutine(AddValue(m_CurFrontValue, newValue, wait));
+            }
+            else
+            {
+                //数值相等情况,直接赋值
+                if (FrontSlider is Image)
+                {
+                    m_Front_Image = FrontSlider as Image;
+                    m_Front_Image.fillAmount = newValue;
+                }
+                else if (FrontSlider is Scrollbar)
+                {
+                    m_Front_Scrollbar = FrontSlider as Scrollbar;
+                    m_Front_Scrollbar.size = newValue;
+                }
+
+                BackSlider.fillAmount = newValue;
+                m_CurBackValue = newValue;
+                m_CurFrontValue = newValue;
+            }
+        }
+        //------------------------------------------------------
+        IEnumerator AddValue(float oldValue, float newValue, WaitForSeconds wait)
+        {
+            //先让背景滑动条到最终值,
             BackSlider.fillAmount = newValue;
             m_CurBackValue = newValue;
-            m_CurFrontValue = newValue;
+            //再过渡前置血条
+            for (int i = 1; i <= LerpCount; i++)
+            {
+                if (m_Front_Image)
+                {
+                    m_CurFrontValue = Mathf.Lerp(oldValue, newValue, (float)i/LerpCount);
+                    m_Front_Image.fillAmount = m_CurFrontValue;
+                }
+                if (m_Front_Scrollbar)
+                {
+                    m_CurFrontValue = Mathf.Lerp(oldValue, newValue, (float)i / LerpCount);
+                    m_Front_Scrollbar.size = m_CurFrontValue;
+                }
+                yield return wait;
+            }
         }
-    }
-
-    IEnumerator AddValue(float oldValue, float newValue, WaitForSeconds wait)
-    {
-        //先让背景滑动条到最终值,
-        BackSlider.fillAmount = newValue;
-        m_CurBackValue = newValue;
-        //再过渡前置血条
-        for (int i = 1; i <= LerpCount; i++)
+        //------------------------------------------------------
+        IEnumerator SubValue(float oldValue, float newValue,WaitForSeconds wait)
         {
+            //先让前置血条过渡到最终值
             if (m_Front_Image)
             {
-                m_CurFrontValue = Mathf.Lerp(oldValue, newValue, (float)i / LerpCount);
+                m_Front_Image.fillAmount = newValue;
+            }
+            if (m_Front_Scrollbar)
+            {
+                m_Front_Scrollbar.size = newValue;
+            }
+            m_CurFrontValue = newValue;
+            //再过渡后置血条
+            for (int i = 1; i <= LerpCount; i++)
+            {
+                m_CurBackValue = Mathf.Lerp(oldValue, newValue, (float)i / LerpCount);
+                BackSlider.fillAmount = m_CurBackValue;
+                yield return wait;
+            }
+        }
+        //------------------------------------------------------
+        private void OnDestroy()
+        {
+            Clear();
+            m_LerpCoroutine = null;
+            BackSlider = null;
+            FrontSlider = null;
+            m_Front_Image = null;
+            m_Front_Scrollbar = null;
+            
+        }
+        //------------------------------------------------------
+        public void Clear()
+        {
+            if (m_LerpCoroutine != null)
+            {
+                StopCoroutine(m_LerpCoroutine);
+                m_LerpCoroutine = null;
+            }
+
+            m_CurBackValue = 1f;
+            m_CurFrontValue = 1f;
+            m_SliderValue = 1f;
+            LerpSpeed = 1f;
+            if (m_Front_Image)
+            {
                 m_Front_Image.fillAmount = m_CurFrontValue;
             }
             if (m_Front_Scrollbar)
             {
-                m_CurFrontValue = Mathf.Lerp(oldValue, newValue, (float)i / LerpCount);
                 m_Front_Scrollbar.size = m_CurFrontValue;
             }
-            yield return wait;
-        }
-    }
+            if (BackSlider)
+            {
+                BackSlider.fillAmount = m_CurBackValue;
+            }
 
-    IEnumerator SubValue(float oldValue, float newValue, WaitForSeconds wait)
-    {
-        //先让前置血条过渡到最终值
-        if (m_Front_Image)
-        {
-            m_Front_Image.fillAmount = newValue;
-        }
-        if (m_Front_Scrollbar)
-        {
-            m_Front_Scrollbar.size = newValue;
-        }
-        m_CurFrontValue = newValue;
-        //再过渡后置血条
-        for (int i = 1; i <= LerpCount; i++)
-        {
-            m_CurBackValue = Mathf.Lerp(oldValue, newValue, (float)i / LerpCount);
-            BackSlider.fillAmount = m_CurBackValue;
-            yield return wait;
-        }
-    }
-
-    private void OnDestroy()
-    {
-        Clear();
-        m_LerpCoroutine = null;
-        BackSlider = null;
-        FrontSlider = null;
-        m_Front_Image = null;
-        m_Front_Scrollbar = null;
-
-    }
-
-    public void Clear()
-    {
-        if (m_LerpCoroutine != null)
-        {
-            StopCoroutine(m_LerpCoroutine);
-            m_LerpCoroutine = null;
-        }
-
-        m_CurBackValue = 1f;
-        m_CurFrontValue = 1f;
-        m_SliderValue = 1f;
-        LerpSpeed = 1f;
-        if (m_Front_Image)
-        {
-            m_Front_Image.fillAmount = m_CurFrontValue;
-        }
-        if (m_Front_Scrollbar)
-        {
-            m_Front_Scrollbar.size = m_CurFrontValue;
-        }
-        if (BackSlider)
-        {
-            BackSlider.fillAmount = m_CurBackValue;
+            m_IsActive = false;
         }
     }
 }
