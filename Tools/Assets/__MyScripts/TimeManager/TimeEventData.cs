4149e0041112e1a284e1f59d82980fa147cb8701
newwer <1554263906@qq.com>
2020-12-13 23:46:39 +0800

    增加时间事件数据结构

---
+++ Tools/Assets/__MyScripts/TimeManager/TimeEventData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public struct TimeEventData 
+{
+    /// <summary>
+    /// 设置的日期,用DateTime就可以表示
+    /// </summary>
+    public DateTime date;
+    /// <summary>
+    /// 显示的提示语句
+    /// </summary>
+    public string showTips;
+    /// <summary>
+    /// 播放的音频
+    /// </summary>
+    public AudioClip playAudio;
+
+    public TimeEventData(DateTime date, string showTips, AudioClip playAudio)
+    {
+        this.date = date;
+        this.showTips = showTips ?? throw new ArgumentNullException(nameof(showTips));
+        this.playAudio = playAudio;
+    }
+}
