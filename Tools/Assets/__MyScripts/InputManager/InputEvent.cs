01ec13286013fc5122f44344b8f1f128764ffc57
DESKTOP-1NV8NIM\TZ03 <1554263906@qq.com>
2020-12-28 16:42:12 +0800

    输入事件的监听完善

--- Tools/Assets/__MyScripts/InputManager/InputEvent.cs
+++ Tools/Assets/__MyScripts/InputManager/InputEvent.cs
@@ -1,95 +1,128 @@
-﻿using System.Collections;
+﻿/********************************************************************
+生成日期:	6:17:2020 15:16
+类    名: 	InputEvent
+作    者:	zdq
+描    述:	输入监听事件
+*********************************************************************/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class InputEvent : MonoBehaviour,IPointerClickHandler,IBeginDragHandler,IDragHandler,IEndDragHandler,IPointerDownHandler,IPointerUpHandler
+namespace TopGame.UI
 {
-    [Range(0.01f,2f)]
-    public float DragRotateTime = 0.5f;
-    float m_PointDownTime;
-
-    
-    void OnNext(PointerEventData eventData)
+    public class InputEvent : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler, IPointerUpHandler
     {
-        Vector3 delta = eventData.pressPosition - eventData.position;
-        Debug.Log("按下时位置:" + eventData.pressPosition + ",现在位置:" + eventData.position + ",delta:" + delta);
-        if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+        [Range(0.01f, 2f)]
+        public float DragRotateTime = 0.5f;
+
+        public System.Action OnSwapLeft;
+        public System.Action OnSwapRight;
+        public System.Action OnRotationBegin;
+        public System.Action<float> OnRotation;
+        public System.Action OnRotationEnd;
+        public System.Action OnClick;
+
+        float m_PointDownTime;
+        bool m_IsRotation = false;
+
+
+        void OnNext(PointerEventData eventData)
         {
-            //Debug.Log("左右滑动");
-            if (delta.x > 0)//左下角(0,0)点,右上角(屏幕分辨率)
+            Vector3 delta = eventData.pressPosition - eventData.position;
+            //Debug.Log("按下时位置:" + eventData.pressPosition + ",现在位置:" + eventData.position + ",delta:" + delta);
+            if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
             {
-                Debug.Log("右滑动");
+                //Debug.Log("左右滑动");
+                if (delta.x > 0)//左下角(0,0)点,右上角(屏幕分辨率)
+                {
+                    //Debug.Log("右滑动");
+                    OnSwapRight?.Invoke();
+                }
+                else
+                {
+                    //Debug.Log("左滑动");
+                    OnSwapLeft?.Invoke();
+                }
             }
             else
             {
-                Debug.Log("左滑动");
+                //Debug.Log("上下滑动");
+                if (delta.y > 0)//从左下角开始
+                {
+                    //Debug.Log("下滑动");
+                }
+                else
+                {
+                    //Debug.Log("上滑动");
+                }
             }
         }
-        else
+
+        void OnRotateBegin()
         {
-            //Debug.Log("上下滑动");
-            if (delta.y > 0)//从左下角开始
+            //Debug.Log("旋转开始");
+            m_IsRotation = true;
+            OnRotationBegin?.Invoke();
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            //Debug.Log("拖拽开始,time:" + Time.unscaledTime);
+            //这边根据按下时停留的时间判断是滑动操作还是旋转操作
+            if (Time.unscaledTime - m_PointDownTime >= DragRotateTime)
             {
-                Debug.Log("下滑动");
+                OnRotateBegin();
             }
             else
             {
-                Debug.Log("上滑动");
+                OnNext(eventData);
             }
         }
-    }
 
-    void OnRotate()
-    {
-        Debug.Log("旋转");
-    }
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Debug.Log("拖拽开始,time:" + Time.unscaledTime);
-        //这边根据按下时停留的时间判断是滑动操作还是旋转操作
-        if (Time.unscaledTime - m_PointDownTime >= DragRotateTime)
+        public void OnDrag(PointerEventData eventData)
         {
-            OnRotate();
+            //Debug.Log("拖拽中");
+            if (m_IsRotation)
+            {
+                OnRotation?.Invoke(eventData.delta.x);
+            }
         }
-        else
+
+        public void OnEndDrag(PointerEventData eventData)
         {
-            OnNext(eventData);
+            //Debug.Log("拖拽结束");
+            if (m_IsRotation)
+            {
+                OnRotationEnd?.Invoke();
+            }
+            m_IsRotation = false;
+            
         }
-    }
 
-    public void OnDrag(PointerEventData eventData)
-    {
-        Debug.Log("拖拽中");
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("拖拽结束");
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (eventData.dragging)
+        public void OnPointerClick(PointerEventData eventData)
         {
-            //拖拽的操作就不执行点击事件
-            Debug.Log("拖拽操作覆盖点击");
+            if (eventData.dragging)
+            {
+                //拖拽的操作就不执行点击事件
+                //Debug.Log("拖拽操作覆盖点击");
+            }
+            else
+            {
+                //Debug.Log("点击");
+                OnClick?.Invoke();
+            }
         }
-        else
+
+        public void OnPointerDown(PointerEventData eventData)
         {
-            Debug.Log("点击");
+            m_PointDownTime = Time.unscaledTime;
+            //Debug.Log("按下,time:" + Time.unscaledTime);
         }
-    }
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        m_PointDownTime = Time.unscaledTime;
-        Debug.Log("按下,time:" + Time.unscaledTime);
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        Debug.Log("松开");
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            //Debug.Log("松开");
+        }
     }
 }
