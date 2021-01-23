bac4d35dabff0bca207f376afa3367453f4d21ad
newwer <1554263906@qq.com>
2021-01-23 17:07:43 +0800

    Input Penetrate

---
+++ Tools/Assets/__MyScripts/InputManager/InputPenetrate/InputPenetrate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace InputModule
+{
+    /// <summary>
+    /// 鼠标穿透
+    /// </summary>
+    public class InputPenetrate : MonoBehaviour, IPointerClickHandler
+    {
+        public GraphicRaycaster gr;
+        List<RaycastResult> m_Results = new List<RaycastResult>();
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            gr.Raycast(eventData, m_Results);
+            foreach (var item in m_Results)
+            {
+                if (name == item.gameObject.name)
+                {
+                    continue;
+                }
+                //print(item.gameObject.name);
+                UIEventListener.Get(item.gameObject).onPointerClick?.Invoke(item.gameObject);
+            }
+        }
+        
+    }
+}
---
+++ Tools/Assets/__MyScripts/TestClass/TestInputPrnetrate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    /// <summary>
+    /// 测试鼠标穿透
+    /// </summary>
+    public class TestInputPrnetrate : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            UIEventListener.Get(gameObject).onPointerClick = OnClick;
+        }
+
+        private void OnClick(GameObject go)
+        {
+            print("click " + name);
+        }
+    }
+}
