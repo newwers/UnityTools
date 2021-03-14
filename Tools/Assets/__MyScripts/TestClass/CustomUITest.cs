f36299613f1254fe496c90a807da87c95b05cd58
newwer <1554263906@qq.com>
2021-03-14 18:11:54 +0800

    自定义UI 血条提交

---
+++ Tools/Assets/__MyScripts/TestClass/CustomUITest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using zdq.UI;
+
+namespace zdq.Test
+{
+    public class CustomUITest : MonoBehaviour
+    {
+        public Slider slider;
+        public HPCustomUI customUI;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            slider.onValueChanged.AddListener(OnValueChange);
+            Init();
+        }
+
+        private void Init()
+        {
+            if (customUI)
+            {
+                customUI.m_hp = slider.value;
+            }
+        }
+
+        private void OnValueChange(float value)
+        {
+            if (customUI)
+            {
+                customUI.m_hp = value;
+            }
+        }
+
+    }
+}
