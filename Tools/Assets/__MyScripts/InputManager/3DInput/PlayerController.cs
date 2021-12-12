8b655eaf6549292154d949fdf4d4a09d2c807751
newwer <1554263906@qq.com>
2021-12-12 20:37:43 +0800

    添加3d输入控制脚本

---
+++ Tools/Assets/__MyScripts/InputManager/3DInput/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace zdq.Zombie
+{
+    /// <summary>
+    /// 基础控制
+    /// </summary>
+    public class PlayerController : MonoBehaviour
+    {
+        public Transform MoveTarget;
+        public Transform RotateTarget;
+
+        public float MoveSpeed = 10f;
+        public float RotateSpeed = 5f;
+
+        
+
+
+        Vector3 m_MouseInput;
+        Vector3 m_MoveDir;
+
+
+        private void Awake()
+        {
+            if (MoveTarget == null)
+            {
+                MoveTarget = transform;
+            }
+            if (RotateTarget == null)
+            {
+                RotateTarget = transform;
+            }
+        }
+
+        private void Update()
+        {
+            m_MouseInput.x = Input.GetAxis("Mouse X");
+            m_MouseInput.y = Input.GetAxis("Mouse Y");
+            m_MouseInput.z = 0;
+            m_MoveDir.x = Input.GetAxis("Horizontal");
+            m_MoveDir.z = Input.GetAxis("Vertical");
+
+            MoveTarget.position += MoveTarget.forward * m_MoveDir.z * Time.deltaTime * MoveSpeed;//朝着当前方向前后
+            MoveTarget.position += MoveTarget.right * m_MoveDir.x * Time.deltaTime * MoveSpeed;//当前方向左右
+
+            RotateTarget.Rotate(Vector3.up, m_MouseInput.x * RotateSpeed * Time.deltaTime);//左右旋转
+            RotateTarget.Rotate(Vector3.right, -m_MouseInput.y * RotateSpeed * Time.deltaTime);//上下旋转
+            RotateTarget.rotation = Quaternion.Euler(RotateTarget.rotation.eulerAngles.x, RotateTarget.rotation.eulerAngles.y,0);//锁定z轴
+        }
+
+
+    }
+}
